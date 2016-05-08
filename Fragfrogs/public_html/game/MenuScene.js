edbb81b43962fd8a1eb293db17fa5bee511acfbf
Hugo Mater <Hugo.Mater@hva.nl>
2016-05-08 21:41:17 +0200

    Players can now pick which player color they want to play.
    Visual representation for picking a button in the menu's.
    When a player is ready to start it is now visualized.

--- Fragfrogs/public_html/game/MenuScene.js
+++ Fragfrogs/public_html/game/MenuScene.js
@@ -39,8 +39,14 @@ function MenuScene(engine)
 {
     Scene.call(this);
     
-    var currentMenuState = MENUSTATE.MainMenu;
+    var currentMenuState = MENUSTATE.ReadyCheck;
     var mainMenuChoice = 0;
+    
+    var selectedSprite = new Animation(Engine.currentGame[engine.gameTitle].gameAssets["PlayerGreen"], new Vector(16,16));
+    selectedSprite.frameIndex = 1;
+    var selectedPositionOne = [new Vector(0,0), new Vector(0,0)];
+    var selectedPositionTwo = [new Vector(0,0), new Vector(0,0)];
+    var selectedPosition = 0;
     
     var playerOneReady = false;
     var playerTwoReady = false;
@@ -48,6 +54,8 @@ function MenuScene(engine)
     var levelChosen = 0;
     var playerOneChoice = PLAYER_PICKS.green;
     var playerTwoChoice = PLAYER_PICKS.brown;
+    var playerOneCurrentPick = 0;
+    var playerTwoCurrentPick = 1;
     
     var fragfrogsLogo = new Sprite(Engine.currentGame[engine.gameTitle].gameAssets["MainLogo"]);
     var startButton = new Sprite(Engine.currentGame[engine.gameTitle].gameAssets["Start"]);
@@ -75,6 +83,110 @@ function MenuScene(engine)
     levels[0] = LoadLevel(1, new Vector(10,10));
     levels[1] = LoadLevel(2, new Vector(10,10));
     levels[2] = LoadLevel(3, new Vector(10,10));
+    
+    setInterval(SwitchSelectedPosition, 400);
+    
+    function HandlePlayerOnePick(input)
+    {
+        if(input.keyboard.keyPressed(KEY_CODE.a))
+        {
+            playerOneCurrentPick--;
+            if(playerOneCurrentPick < 0)
+            {
+                playerOneCurrentPick = 7;
+            }
+            if(playerOneCurrentPick === playerTwoCurrentPick)
+            {
+                if(playerTwoCurrentPick !== 0)
+                {
+                    playerOneCurrentPick--;
+                }else
+                {
+                    playerOneCurrentPick = 7;
+                }
+            }
+        }else if(input.keyboard.keyPressed(KEY_CODE.d))
+        {
+            playerOneCurrentPick++;
+            if(playerOneCurrentPick > 7)
+            {
+                playerOneCurrentPick = 0;
+            }
+            if(playerOneCurrentPick === playerTwoCurrentPick)
+            {
+                if(playerTwoCurrentPick !== 7)
+                {
+                    playerOneCurrentPick++;
+                }else
+                {
+                    playerOneCurrentPick = 0;
+                }
+            }
+        }else if(input.keyboard.keyPressed(KEY_CODE.w))
+        {
+            if(playerOneCurrentPick - 4 !== playerTwoCurrentPick && playerOneCurrentPick >= 4)
+            {
+                playerOneCurrentPick -= 4;
+            }
+        }else if(input.keyboard.keyPressed(KEY_CODE.s))
+        {
+            if(playerOneCurrentPick + 4 !== playerTwoCurrentPick && playerOneCurrentPick < 4)
+            {
+                playerOneCurrentPick += 4;
+            }
+        }
+    };
+    
+    function HandlePlayerTwoPick(input)
+    {
+        if(input.keyboard.keyPressed(KEY_CODE.LEFT))
+        {
+            playerTwoCurrentPick--;
+            if(playerTwoCurrentPick < 0)
+            {
+                playerTwoCurrentPick = 7;
+            }
+            if(playerTwoCurrentPick === playerOneCurrentPick)
+            {
+                if(playerOneCurrentPick !== 0)
+                {
+                    playerTwoCurrentPick--;
+                }else
+                {
+                    playerTwoCurrentPick = 7;
+                }
+            }
+        }else if(input.keyboard.keyPressed(KEY_CODE.RIGHT))
+        {
+            playerTwoCurrentPick++;
+            if(playerTwoCurrentPick > 7)
+            {
+                playerTwoCurrentPick = 0;
+            }
+            if(playerTwoCurrentPick === playerOneCurrentPick)
+            {
+                if(playerOneCurrentPick !== 7)
+                {
+                    playerTwoCurrentPick++;
+                }else
+                {
+                    playerTwoCurrentPick = 0;
+                }
+            }
+        }else if(input.keyboard.keyPressed(KEY_CODE.UP))
+        {
+            if(playerTwoCurrentPick - 4 !== playerOneCurrentPick && playerTwoCurrentPick >= 4)
+            {
+                playerTwoCurrentPick -= 4;
+            }
+        }else if(input.keyboard.keyPressed(KEY_CODE.DOWN))
+        {
+            if(playerTwoCurrentPick + 4 !== playerOneCurrentPick && playerTwoCurrentPick < 4)
+            {
+                playerTwoCurrentPick += 4;
+            }
+        }
+    };
     
     MenuScene.prototype.Update = function(input, dt)
     {
@@ -140,6 +252,8 @@ function MenuScene(engine)
                     playerTwoChoice = PLAYER_PICKS.brown;
                     currentMenuState = MENUSTATE.PickLevel ;
                 }
+                HandlePlayerOnePick(input);
+                HandlePlayerTwoPick(input);
                 break;
             case MENUSTATE.ReadyCheck:
                 if(input.keyboard.keyPressed(KEY_CODE.SPACE))
@@ -221,11 +335,27 @@ function MenuScene(engine)
         return spriteList;
     };
     
+    function DrawSelectedButton(context)
+    {
+        selectedSprite.Draw(context, selectedPositionOne[selectedPosition], 90, new Vector(1,1));
+        selectedSprite.Draw(context, selectedPositionTwo[selectedPosition], 270, new Vector(1,1));
+    };
+    
     function DrawMainMenu(context)
     {
         fragfrogsLogo.Draw(context, new Vector(200,50), 0, new Vector(1,1));
         startButton.Draw(context, new Vector(200,150), 0, new Vector(1,1));
         instructionButton.Draw(context, new Vector(200,200), 0, new Vector(1,1));
+        if(mainMenuChoice === 0)
+        {
+            selectedPositionOne = [new Vector(128,150), new Vector(96,150)];
+            selectedPositionTwo = [new Vector(272,150), new Vector(304,150)];
+        }else
+        {
+            selectedPositionOne = [new Vector(128,200), new Vector(96,200)];
+            selectedPositionTwo = [new Vector(272,200), new Vector(304,200)];
+        }
+        DrawSelectedButton(context);
     };
     
     function DrawLevelChoice(context)
@@ -247,12 +377,13 @@ function MenuScene(engine)
         context.restore();
         
         startButton.Draw(context, new Vector(200,260), 0, new Vector(1,1));
+        selectedPositionOne = [new Vector(128,260), new Vector(96,260)];
+        selectedPositionTwo = [new Vector(272,260), new Vector(304,260)];
+        DrawSelectedButton(context);
     };
     
     function DrawPlayerChoice(context)
     {
-        startButton.Draw(context, new Vector(200,260), 0, new Vector(1,1));
-        
         optionGreen.Draw(context, new Vector(146,110), 0, new Vector(1,1));
         optionBrown.Draw(context, new Vector(182,110), 0, new Vector(1,1));
         optionBlue.Draw(context, new Vector(218,110), 0, new Vector(1,1));
@@ -262,8 +393,99 @@ function MenuScene(engine)
         optionSkyBlue.Draw(context, new Vector(218,150), 0, new Vector(1,1));
         optionYellow.Draw(context, new Vector(254,150), 0, new Vector(1,1));
         
-        p1.Draw(context, new Vector(146,110), 0, new Vector(1,1));
-        p2.Draw(context, new Vector(182,110), 0, new Vector(1,1));
+        var p1Position = new Vector(0,0);
+        var p2Position = new Vector(0,0);
+        
+        switch(playerOneCurrentPick)
+        {
+            case 0:
+                p1Position = new Vector(146,110);
+                playerChoiceOne = optionGreen;
+                playerOneChoice = PLAYER_PICKS.green;
+                break;
+            case 1:
+                p1Position = new Vector(182,110);
+                playerChoiceOne = optionBrown;
+                playerOneChoice = PLAYER_PICKS.brown;
+                break;
+            case 2:
+                p1Position = new Vector(218,110);
+                playerChoiceOne = optionBlue;
+                playerOneChoice = PLAYER_PICKS.blue;
+                break;
+            case 3:
+                p1Position = new Vector(254,110);
+                playerChoiceOne = optionGray;
+                playerOneChoice = PLAYER_PICKS.gray;
+                break;
+            case 4:
+                p1Position = new Vector(146,150);
+                playerChoiceOne = optionPink;
+                playerOneChoice = PLAYER_PICKS.pink;
+                break;
+            case 5:
+                p1Position = new Vector(182,150);
+                playerChoiceOne = optionRed;
+                playerOneChoice = PLAYER_PICKS.red;
+                break;
+            case 6:
+                p1Position = new Vector(218,150);
+                playerChoiceOne = optionSkyBlue;
+                playerOneChoice = PLAYER_PICKS.skyBlue;
+                break;
+            case 7:
+                p1Position = new Vector(254,150);
+                playerChoiceOne = optionYellow;
+                playerOneChoice = PLAYER_PICKS.yellow;
+                break;
+        }
+        
+        switch(playerTwoCurrentPick)
+        {
+            case 0:
+                p2Position = new Vector(146,110);
+                playerChoiceTwo = optionGreen;
+                playerTwoChoice = PLAYER_PICKS.green;
+                break;
+            case 1:
+                p2Position = new Vector(182,110);
+                playerChoiceTwo = optionBrown;
+                playerTwoChoice = PLAYER_PICKS.brown;
+                break;
+            case 2:
+                p2Position = new Vector(218,110);
+                playerChoiceTwo = optionBlue;
+                playerTwoChoice = PLAYER_PICKS.blue;
+                break;
+            case 3:
+                p2Position = new Vector(254,110);
+                playerChoiceTwo = optionGray;
+                playerTwoChoice = PLAYER_PICKS.gray;
+                break;
+            case 4:
+                p2Position = new Vector(146,150);
+                playerChoiceTwo = optionPink;
+                playerTwoChoice = PLAYER_PICKS.pink;
+                break;
+            case 5:
+                p2Position = new Vector(182,150);
+                playerChoiceTwo = optionRed;
+                playerTwoChoice = PLAYER_PICKS.red;
+                break;
+            case 6:
+                p2Position = new Vector(218,150);
+                playerChoiceTwo = optionSkyBlue;
+                playerTwoChoice = PLAYER_PICKS.skyBlue;
+                break;
+            case 7:
+                p2Position = new Vector(254,150);
+                playerChoiceTwo = optionYellow;
+                playerTwoChoice = PLAYER_PICKS.yellow;
+                break;
+        }
+        
+        p1.Draw(context, p1Position, 0, new Vector(1,1));
+        p2.Draw(context, p2Position, 0, new Vector(1,1));
         
         playerChoiceOne.Draw(context, new Vector(114, 50), 0, new Vector(1,1));
         playerChoiceTwo.Draw(context, new Vector(288, 50), 0, new Vector(1,1));
@@ -277,8 +499,12 @@ function MenuScene(engine)
         context.font = "10px Arial";
         context.fillStyle = "white";
         context.fillText("Press WASD or use the arrow keys to change character", 70, 190);
-        
         context.restore();
+        
+        startButton.Draw(context, new Vector(200,260), 0, new Vector(1,1));
+        selectedPositionOne = [new Vector(128,260), new Vector(96,260)];
+        selectedPositionTwo = [new Vector(272,260), new Vector(304,260)];
+        DrawSelectedButton(context);
     };
     
     function DrawReadyCheck(context)
@@ -293,6 +519,27 @@ function MenuScene(engine)
         context.fillStyle = "white";
         context.fillText("Shoot to ready", 165, 230);
         context.restore();
+        
+        if(playerOneReady) 
+        {
+            selectedSprite.Draw(context, new Vector(70, 267), 90, new Vector(0.75, 0.75));
+            context.save();
+            context.font = "10px Arial";
+            context.fillStyle = "white";
+            context.fillText("Ready", 82, 270);
+            context.restore();
+            selectedSprite.Draw(context, new Vector(125, 267), 270, new Vector(0.75, 0.75));
+        }
+        if(playerTwoReady)
+        {
+            selectedSprite.Draw(context, new Vector(276, 267), 90, new Vector(0.75, 0.75));
+            context.save();
+            context.font = "10px Arial";
+            context.fillStyle = "white";
+            context.fillText("Ready", 288, 270);
+            context.restore();
+            selectedSprite.Draw(context, new Vector(331, 267), 270, new Vector(0.75, 0.75));
+        }
     };
     
     function DrawControls(context)
@@ -300,7 +547,16 @@ function MenuScene(engine)
         instructionsImage.Draw(context, new Vector(200, 160), 0, new Vector(1,1));
     };
     
-    
+    function SwitchSelectedPosition()
+    {
+        if(selectedPosition === 0)
+        {
+            selectedPosition = 1;
+        }else
+        {
+            selectedPosition = 0;
+        }
+    };
 }
 
 MenuScene.prototype = Object.create(Scene.prototype);
