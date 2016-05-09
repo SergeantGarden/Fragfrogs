ef0030ad247a74a546c0d07dea78c3c455a05a34
Hugo Mater <Hugo.Mater@hva.nl>
2016-05-09 13:08:21 +0200

    Added particles to the endscene and made the player visualization bigger.

--- Fragfrogs/public_html/game/EndScene.js
+++ Fragfrogs/public_html/game/EndScene.js
@@ -23,10 +23,20 @@ function EndScene(engine, player)
     var selectedPositionOne = [new Vector(128,270), new Vector(96,270)];
     var selectedPositionTwo = [new Vector(272,270), new Vector(304,270)];
     
-    var selectedSprite = new Animation(Engine.currentGame[engine.gameTitle].gameAssets[player], new Vector(16,16));
+    var selectedSprite = new Animation(Engine.currentGame[engine.gameTitle].gameAssets["PlayerGreen"], new Vector(16,16));
     selectedSprite.frameIndex = 1;
     
+    var playerSprite = new Animation(Engine.currentGame[engine.gameTitle].gameAssets[player], new Vector(16,16));
+    playerSprite.frameIndex = 1;
+    
+    var _emitterOne = new Emitter(new Vector(0, 0), true, new Vector(30,30));
+    this.AddGameObject(_emitterOne, "foreground");
+    
+    var _emitterTwo = new Emitter(new Vector(400, 0), true, new Vector(-30,30));
+    this.AddGameObject(_emitterTwo, "foreground");
+    
     setInterval(SwitchSelectedPosition, 400);
+    setInterval(spawnParticles, 100);
     
     EndScene.prototype.Update = function(input, dt)
     {
@@ -37,16 +47,20 @@ function EndScene(engine, player)
                 engine.switchScene(new MenuScene(engine), false);
             }
         }
+        Scene.prototype.Update.call(this, input, dt);
     };
     
     EndScene.prototype.Draw = function(context)
     {
+        
         context.save();
         context.fillStyle = "black";
         context.fillRect(0, 0, Engine.currentGame[engine.gameTitle].originalResolution.x, Engine.currentGame[engine.gameTitle].originalResolution.y);
         context.restore();
         
-        selectedSprite.Draw(context, new Vector(200, 110), 0, new Vector(1,1));
+        Scene.prototype.Draw.call(this, context);
+        
+        playerSprite.Draw(context, new Vector(200, 80), 0, new Vector(2,2));
         
         context.save();
         context.font = "30px Arial";
@@ -68,6 +82,12 @@ function EndScene(engine, player)
         {
             selectedPosition = 0;
         }
+    };
+    
+    function spawnParticles()
+    {
+        _emitterOne.EmitRandom(playerSprite, 5, 10, new Vector(10,10), new Vector(0.5, 0.5));
+        _emitterTwo.EmitRandom(playerSprite, 5, 10, new Vector(10,10), new Vector(0.5, 0.5));
     };
 }
 
