22d4a03b9846cee4c8984ad949768306502153f5
Hugo Mater <Hugo.Mater@hva.nl>
2016-05-09 11:32:53 +0200

    Now has a visualization of the winner.
    TO DO: add music to the endscene, scale the size of the player representation.

--- Fragfrogs/public_html/game/EndScene.js
+++ Fragfrogs/public_html/game/EndScene.js
@@ -18,12 +18,55 @@ function EndScene(engine, player)
 {
     Scene.call(this);
     
+    var continueButton = new Sprite(Engine.currentGame[engine.gameTitle].gameAssets["Continue"]);
+    var selectedPosition = 0;
+    var selectedPositionOne = [new Vector(128,270), new Vector(96,270)];
+    var selectedPositionTwo = [new Vector(272,270), new Vector(304,270)];
+    
+    var selectedSprite = new Animation(Engine.currentGame[engine.gameTitle].gameAssets[player], new Vector(16,16));
+    selectedSprite.frameIndex = 1;
+    
+    setInterval(SwitchSelectedPosition, 400);
+    
     EndScene.prototype.Update = function(input, dt)
     {
         if(input.keyboard.keyDown(KEY_CODE.SPACE))
         {
-            console.log("EndScene");
-            engine.switchOldScene(false);
+            if(!engine.switchOldScene(false))
+            {
+                engine.switchScene(new MenuScene(engine), false);
+            }
+        }
+    };
+    
+    EndScene.prototype.Draw = function(context)
+    {
+        context.save();
+        context.fillStyle = "black";
+        context.fillRect(0, 0, Engine.currentGame[engine.gameTitle].originalResolution.x, Engine.currentGame[engine.gameTitle].originalResolution.y);
+        context.restore();
+        
+        selectedSprite.Draw(context, new Vector(200, 110), 0, new Vector(1,1));
+        
+        context.save();
+        context.font = "30px Arial";
+        context.fillStyle = "white";
+        context.fillText("You won the game!", 70, 210);
+        context.restore();
+        
+        continueButton.Draw(context, new Vector(200,270), 0, new Vector(1,1));
+        selectedSprite.Draw(context, selectedPositionOne[selectedPosition], 90, new Vector(1,1));
+        selectedSprite.Draw(context, selectedPositionTwo[selectedPosition], 270, new Vector(1,1));
+    };
+    
+    function SwitchSelectedPosition()
+    {
+        if(selectedPosition === 0)
+        {
+            selectedPosition = 1;
+        }else
+        {
+            selectedPosition = 0;
         }
     };
 }
